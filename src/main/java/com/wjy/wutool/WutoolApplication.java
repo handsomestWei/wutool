5dcf818121add3e3bdae3aa64f77bc37010234d3
handsomestWei <1293986765@qq.com>
2025-02-22 00:00:08 +0800

    [add] 增加配置文件热更新模块使用说明

---
+++ src/main/java/com/wjy/wutool/WutoolApplication.java
@@ -0,0 +1,13 @@
+package com.wjy.wutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WutoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WutoolApplication.class, args);
+    }
+
+}
