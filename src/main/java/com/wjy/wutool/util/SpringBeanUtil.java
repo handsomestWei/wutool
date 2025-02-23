ddae11e1053f7833715c7c60f64156eb433a5b94
handsomestWei <1293986765@qq.com>
2025-02-23 19:32:47 +0800

    [add] 添加spring bean工具类

---
+++ src/main/java/com/wjy/wutool/util/SpringBeanUtil.java
@@ -0,0 +1,35 @@
+package com.wjy.wutool.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * 从spring上下文容器获取指定bean
+ */
+@Component
+public class SpringBeanUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringBeanUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) throws BeansException {
+        return (T)applicationContext.getBean(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<?> clz) throws BeansException {
+        return (T)applicationContext.getBean(clz);
+    }
+
+}
